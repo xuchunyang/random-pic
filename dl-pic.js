c7571496806892ef1b528b7dc14615c85ce3f857
Xu Chunyang <xuchunyang56@gmail.com>
2020-12-03 00:17:30 +0800

    * dl-pic.js: 删除 imageJson，不必要

--- dl-pic.js
+++ dl-pic.js
@@ -15,14 +15,6 @@ if (!fs.existsSync(imageDir)) {
   fs.mkdirSync(imageDir);
 }
 
-// 图片下载记录
-const imageJson = path.join(dataDir, "image.json");
-let imageData = {};
-if (fs.existsSync(imageJson)) {
-  debug("%s 存在，初始化图片下载记录 ", imageJson);
-  data = JSON.parse(fs.readFileSync(imageJson, "utf8"));
-}
-
 debug("共有 %d 个图片要下载", Object.keys(data).length);
 
 // 图片大小：original > thumbnail > preferred
@@ -32,41 +24,28 @@ debug("共有 %d 个图片要下载", Object.keys(data).length);
   for (let key in data) {
     // key is like "2020-12-02"
     debug("[%d/%d]", i++, Object.keys(data).length);
-    if (key in imageData) {
-      debug("已经处理过 %s，跳过", key);
-      continue;
-    }
     if ("error" in data) {
       debug("跳过 %s, 没有图片, %o", data.error);
-      imageData[key] = {
-        ok: false,
-        error: data.error,
-      };
     } else if ("preferred" in data[key]) {
       const url = data[key].preferred.url;
+      const filename = path.join(imageDir, md5(url));
+      if (fs.existsSync(filename)) {
+        debug("图片 %s 已经存在，跳过", filename);
+        continue;
+      }
       debug("下载图片, %s", url);
       try {
-        const response = await axios(url, { responseType: "stream" });
-        const filename = path.join(imageDir, md5(url));
+        const response = await axios(url, {
+          responseType: "stream",
+          timeout: 10 * 1000,
+        });
         debug("图片将要保存到 %s", filename);
         response.data.pipe(fs.createWriteStream(filename));
-        imageData[key] = {
-          ok: true,
-          filename,
-        };
       } catch (error) {
         debug("跳过，请求遇到问题：%s", error.message);
-        imageData[key] = {
-          ok: false,
-          error: error.message,
-        };
       }
     } else {
       debug("跳过 %s, 有问题，缺少 preferred 数据，%o", data);
-      imageData[key] = {
-        ok: false,
-        error: "缺少 preferred 数据",
-      };
     }
   }
 })();
